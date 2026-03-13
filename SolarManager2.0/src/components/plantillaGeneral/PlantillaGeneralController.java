5aa7b0569e9b2a0fecdadd1884207fc2294bd679
Ivan <ivangalannieto@gmail.com>
2026-03-13 13:00:41 +0100

    Plantillas y componentes

---
+++ SolarManager2.0/src/components/plantillaGeneral/PlantillaGeneralController.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package components.plantillaGeneral;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author ivang
+ */
+public class PlantillaGeneralController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
